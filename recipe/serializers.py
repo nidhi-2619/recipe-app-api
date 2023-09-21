75ea986ca687d7afaa209bdfa2b85fe192e2a125
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2023-09-21 16:29:25 +0530

    test(tags): test for tags added and tags logic implemented to views and mapped with url

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -3,7 +3,10 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -27,3 +30,11 @@ class RecipeDetailsSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields
 
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
